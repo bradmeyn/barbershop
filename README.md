add1932da09d0c192f5098a5a35d37370f4b1056
bradmeyn <bradjmeyn@gmail.com>
2023-11-03 23:30:46 +1100

    hero and pricing sections

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Cool Barbershop Site built with Astro
+
+This is a cool barbershop site built with Astro. It's a work in progress.
---
+++ src/consts.ts
@@ -0,0 +1,29 @@
+interface Service {
+  name: string;
+  description: string;
+  price: number;
+}
+
+export const services = [
+  {
+    name: "Haircut",
+    description:
+      "A good haircut is the foundation of any good hairstyle. We offer a variety of haircuts to suit your needs.",
+    price: 15,
+  },
+  {
+    name: "Skin Fade",
+    description: "Skin Fade",
+    price: 20,
+  },
+  {
+    name: "Beard Trim (clipper)",
+    description: "Beard Trim (clipper)",
+    price: 10,
+  },
+  {
+    name: "Hot Towel Shave",
+    description: "Hot Towel Shave",
+    price: 15,
+  },
+];
